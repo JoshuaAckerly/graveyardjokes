3144fb14e5aaedd76d63795ea4d5e960679ff490
Joshua Ackerly <ackerlyj05@gmail.com>
2025-08-05 11:09:03 -0400

    Changes to portfolio

---
+++ resources/js/pages/portfolio.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Portfolio: React.FC = () => {
+    return (
+        <iframe
+            src="https://thevelvetpulse.graveyardjokes.com/"
+            style={{ width: '100%', height: '800px', border: 'none' }}>
+        </iframe>
+
+    );
+};
+
+export default Portfolio;
