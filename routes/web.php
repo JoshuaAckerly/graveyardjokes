af8ddd97989927118cecea0700092ee6f0b1c007
Joshua Ackerly <ackerlyj05@gmail.com>
2025-08-05 06:38:52 -0400

    Added Site Map Generator

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
 
 Route::get('/', function () {
     return Inertia::render('welcome');
@@ -21,6 +23,20 @@ Route::post('/contact', [ContactController::class, 'store'])->name('contact.stor
 Route::get('/terms', fn () => Inertia::render('legal/terms'))->name('terms');
 Route::get('/privacy', fn () => Inertia::render('legal/privacy'))->name('privacy');
 Route::get('/cookies', fn () => Inertia::render('legal/cookies'))->name('cookies');
+
+Route::get('/generate-sitemap', function () {
+    Sitemap::create()
+        ->add(Url::create('/'))
+        ->add(Url::create('/about'))
+        ->add(Url::create('/contact'))
+        ->writeToFile(public_path('sitemap.xml'));
+
+    return 'Sitemap generated!';
+});
+
+Route::fallback(function () {
+    abort(404);
+});
 
 
 require __DIR__.'/settings.php';
