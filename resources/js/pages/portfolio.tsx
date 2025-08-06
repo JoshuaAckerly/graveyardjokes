72ae256318fdd5e54f8d3121d3eb5f27b3ffb5f5
Joshua Ackerly <ackerlyj05@gmail.com>
2025-08-06 00:15:28 -0400

    Changes to portfolio

--- resources/js/pages/portfolio.tsx
+++ resources/js/pages/portfolio.tsx
@@ -4,33 +4,38 @@ import React from 'react';
 const Portfolio: React.FC = () => {
     return (
         <MainLayout>
-            <section className="flex flex-col gap-4 justify-center items-center text-white p-4">
-                <h1>Portfolio</h1>
-                <p className='p-10'>Welcome to my portfolio page! Here you can find a collection of my work and projects that showcase my skills and experience.</p>
-                <ul className="flex flex-col gap-4 text-center justify-center items-center">
+            <section className="flex flex-col gap-4 justify-center items-center text-white p-4 bg-[var(--color-foreground)] rounded-lg shadow-lg">
+                <h1 className='text-5xl text-[var(--card)]'>Portfolio</h1>
+                <p className='p-10 underline w-sm text-center'>Welcome to my portfolio page! Here you can find a collection of my work and projects that showcase my skills and experience.</p>
+                <ul className="flex flex-col gap-4 text-center justify-center items-center w-sm">
                     <li>
+                        <h3 className='text-[var(--card)]'>The Velvet Pulse</h3>
                         <a className="hover:underline" href="https://thevelvetpulse.graveyardjokes.com" target="_blank" rel="noopener noreferrer">
-                            Project 1: A web application that helps users manage their tasks efficiently.
+                            A band site that features music, tour dates, and merchandise.
                         </a>
                     </li>
                     <li>
+                        <h3 className='text-[var(--card)]'>Hollow Press</h3>
                         <a className="hover:underline" href="https://hollowpress.graveyardjokes.com" target="_blank" rel="noopener noreferrer">
-                            Project 2: A blog site that shares articles on various topics including technology, lifestyle, and more.
+                            A blog site that shares articles on various topics including technology, lifestyle, and more.
                         </a>
                     </li>
                     <li>
+                        <h3 className='text-[var(--card)]'>Lunar Blood</h3>
                         <a className="hover:underline" href="https://lunarblood.graveyardjokes.com" target="_blank" rel="noopener noreferrer">
-                            Project 2: a heavy metal band that creates and shares music, merchandise, and updates with fans.
+                            A heavy metal band that creates and shares music, merchandise, and updates with fans.
                         </a>
                     </li>
                     <li>
+                        <h3 className='text-[var(--card)]'>Velvet Radio</h3>
                         <a className="hover:underline" href="https://velvetradio.graveyardjokes.com" target="_blank" rel="noopener noreferrer">
-                            Project 3: A music streaming service that provides users with access to a vast library of songs and playlists.
+                            A music streaming service that provides users with access to a vast library of songs and playlists.
                         </a>
                     </li>
                     <li>
+                        <h3 className='text-[var(--card)]'>Synth Veil</h3>
                         <a className="hover:underline" href="https://synthveil.graveyardjokes.com" target="_blank" rel="noopener noreferrer">
-                            Project 4: A portfolio website that showcases my design and development skills.
+                            A portfolio website that showcases my design and development skills.
                         </a>
                     </li>
                 </ul>
