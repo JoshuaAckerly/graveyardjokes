3190876d95cc065df79f7a264826f9655af72393
Joshua Ackerly <ackerlyj05@gmail.com>
2025-12-15 06:51:26 -0500

    feat: add PayPal integration and refactor services pages
    - Add PayPal SDK integration with donation button component
    
    - Create dedicated service package pages (Starter, Professional, Premium)
    
    - Refactor services page with individual PayPal buttons per package
    
    - Add services section to homepage with Noteleks game description
    
    - Update pricing and improve mobile responsiveness
    
    - Add GitHub open source section to about page
    
    - Add comprehensive PayPal and service packages documentation

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -70,6 +70,9 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
+    {{-- PayPal SDK - Commented out temporarily to fix layout issues --}}
+    {{-- <script src="https://www.paypal.com/sdk/js?client-id=BAAEThXfkghKIa87QQOlnsIur64eOCnBLuAxJeYWYDW5o366RczxK2o9F8DtrXnte6SY65yJRFso_mMA2o&components=hosted-buttons&enable-funding=venmo,paylater&disable-funding=card,credit&currency=USD" data-sdk-integration-source="button-factory"></script> --}}
+
     @routes
     @viteReactRefresh
     @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
