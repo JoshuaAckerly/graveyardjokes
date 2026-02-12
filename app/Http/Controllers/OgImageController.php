b13a5aee74882f901a5283cfa51d0b0c94572858
JoshuaAckerly <32554787+JoshuaAckerly@users.noreply.github.com>
2026-02-12 22:08:06 +0000

    style: auto-fix code style with Laravel Pint

--- app/Http/Controllers/OgImageController.php
+++ app/Http/Controllers/OgImageController.php
@@ -137,7 +137,7 @@ class OgImageController
         if (str_ends_with($host, '.local') || str_ends_with($host, '.localhost')) {
             return false;
         }
-        
+
         $ip = gethostbyname($host);
 
         return ! filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE);
